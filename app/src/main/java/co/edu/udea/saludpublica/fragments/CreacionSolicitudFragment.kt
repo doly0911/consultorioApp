83322527a8ad2cb6120ad3f5d9949d4506e685a5
Doly Jimenez <dolly.jimenez@udea.edu.co>
2020-08-17 02:08:41 -0500

    * Actualizar dependencias * Pasar el objeto solicitud entre fragmentos * Actualizar modelo Solicitud * Activar data-binding para Creación Solicitud

--- app/src/main/java/co/edu/udea/saludpublica/fragments/CreacionSolicitudFragment.kt
+++ app/src/main/java/co/edu/udea/saludpublica/fragments/CreacionSolicitudFragment.kt
@@ -6,24 +6,64 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import co.edu.udea.saludpublica.R
+import co.edu.udea.saludpublica.databinding.FragmentCreacionSolicitudBinding
+import co.edu.udea.saludpublica.enums.MedioRespuestaEnum
+import co.edu.udea.saludpublica.models.Solicitud
+import co.edu.udea.saludpublica.enums.PrioridadEnum
 
 /**
  * A simple [Fragment] subclass.
  */
 class CreacionSolicitudFragment : Fragment() {
 
+    private lateinit var binding : FragmentCreacionSolicitudBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+
+        val args = arguments?.let { CreacionSolicitudFragmentArgs.fromBundle(it) }
+        val solicitud : Solicitud? = args?.solicitud
+
         //Modificar el titulo del actionBar
         (activity as AppCompatActivity).supportActionBar?.title = "Nueva Solicitud"
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_creacion_solicitud, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_creacion_solicitud, container, false)
+
+        binding.spinner.adapter = ArrayAdapter<MedioRespuestaEnum>(
+            context!!,
+            android.R.layout.simple_spinner_dropdown_item,
+            MedioRespuestaEnum.values()
+        )
+        populateActivity(solicitud)
+        return binding.root
+    }
+
+    private fun populateActivity(solicitud: Solicitud?){
+        binding.apply {
+            editTxtAsuntoConsulta.setText(solicitud?.asunto)
+            editTxtDescripcionConsulta.setText(solicitud?.descripcion)
+            when(solicitud?.prioridad){
+                PrioridadEnum.ALTA -> radioBtnPrioridad.check(radioButton6.id)
+                PrioridadEnum.MEDIA -> radioBtnPrioridad.check(radioButton7.id)
+                PrioridadEnum.BAJA -> radioBtnPrioridad.check(radioButton8.id)
+                else -> radioBtnPrioridad.check(radioButton6.id)
+            }
+
+            when(solicitud?.medio){
+                MedioRespuestaEnum.CORREO -> spinner.setSelection(0)
+                MedioRespuestaEnum.TELEFONO-> spinner.setSelection(1)
+                MedioRespuestaEnum.ESCRITO -> spinner.setSelection(2)
+            }
+        }
+
     }
 
 
--- app/src/main/java/co/edu/udea/saludpublica/models/Solciitud.kt
+++ app/src/main/java/co/edu/udea/saludpublica/models/Solciitud.kt
@@ -1,6 +1,10 @@
 package co.edu.udea.saludpublica.models
 
-import co.edu.udea.saludpublica.util.PrioridadEnum
+import android.os.Parcelable
+import co.edu.udea.saludpublica.enums.MedioRespuestaEnum
+import co.edu.udea.saludpublica.enums.PrioridadEnum
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Solicitud(var cedula: String, var persona: String, var asunto: String,
-                     var descripcion: String, var prioridad: PrioridadEnum)
+                     var descripcion: String, var prioridad: PrioridadEnum, var medio : MedioRespuestaEnum) : Parcelable
