e9b520d63318e7621b42ca1210949e01ecb3369c
Doly Jimenez <doly.jimenez11@gmail.com>
2020-08-24 21:32:53 -0500

    resolver conflictos de merge

--- app/src/main/java/co/edu/udea/saludpublica/dao/DefaultSolicitudDao.kt
+++ app/src/main/java/co/edu/udea/saludpublica/dao/DefaultSolicitudDao.kt
@@ -7,13 +7,14 @@ import co.edu.udea.saludpublica.enums.PrioridadEnum
 class DefaultSolicitudDao : SolicitudDao {
     override fun getSolicitudes(): List<Solicitud> {
         val solicitudes = ArrayList<Solicitud>()
-        solicitudes.add(Solicitud("104563312", "Ester Bedoya", "Ayuda", "prueba", PrioridadEnum.BAJA))
-        solicitudes.add(Solicitud("12358963", "Doly Jimenez", "Ayuda", "prueba", PrioridadEnum.ALTA))
-        solicitudes.add(Solicitud("1563393328", "Juana De Arco", "Duda", "prueba", PrioridadEnum.MEDIA))
-        solicitudes.add(Solicitud("657889995", "Jhonatan Orozco", "Ayuda", "prueba", PrioridadEnum.ALTA))
-        solicitudes.add(Solicitud("1151768908", "Pepito Perez", "Ayuda", "prueba", PrioridadEnum.MEDIA))
-        solicitudes.add(Solicitud("156339928", "Bernardo Restrepo", "Donec convallis mauris eget.", "Integer imperdiet sed libero vitae laoreet. Cras consequat nunc a dictum laoreet. Nullam vitae nulla.", PrioridadEnum.MEDIA))
-        solicitudes.add(Solicitud("1567563312", "Florinda Meza", "Ayuda", "prueba", PrioridadEnum.BAJA))
+        solicitudes.add(Solicitud("104563312", "Ester Bedoya", "Ayuda",
+            "prueba", PrioridadEnum.BAJA, MedioRespuestaEnum.CORREO))
+        solicitudes.add(Solicitud("12358963", "Doly Jimenez", "Ayuda",
+            "prueba", PrioridadEnum.ALTA, MedioRespuestaEnum.TELEFONO))
+        solicitudes.add(Solicitud("1563328", "Bernardo Restrepo", "Ayuda",
+            "prueba", PrioridadEnum.MEDIA, MedioRespuestaEnum.ESCRITO))
+        solicitudes.add(Solicitud("1563328", "Jhonatan Orozco", "Ayuda",
+            "prueba", PrioridadEnum.ALTA, MedioRespuestaEnum.CORREO))
         return solicitudes
     }
 
